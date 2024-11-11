8726ae17c52195f04aa718284fa460b16a71088a
David Lyons <david@meshify.com>
2024-11-10 20:42:59 -0600

    copilot chat and sql

--- lua/community.lua
+++ lua/community.lua
@@ -10,6 +10,7 @@ return {
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
   { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.editing-support.copilotchat-nvim" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.pack.go" },
@@ -18,7 +19,7 @@ return {
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.markdown" },
-  --  { import = "astrocommunity.pack.sql" },
+  { import = "astrocommunity.pack.sql" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.yaml" },
 }
