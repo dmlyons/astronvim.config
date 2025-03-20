7c96af5f39fc990dd24ff92ef8f86682ca5306ff
David Lyons <david@meshify.com>
2025-03-20 16:48:20 -0500

    sql is annoying again

--- lua/community.lua
+++ lua/community.lua
@@ -20,8 +20,7 @@ return {
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.markdown" },
-  { import = "astrocommunity.pack.sql" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.yaml" },
-  { import = "astrocommunity.colorscheme.gruvbox-nvim" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
 }
